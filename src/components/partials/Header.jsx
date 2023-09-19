7a461d3dd69f52095633c904250914dbeeeb86c6
Thomas <thomas.mortelette@outlook.fr>
2023-09-19 14:21:20 +0200

    feat: Ajout du logo sur header

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,13 +1,65 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
+
+import { LogoIcon } from "../icons";
 
 const Header = () => {
 
+    const theme = useTheme();
+
     return(
-        <>
-        
-        </>
+        <HeaderContainer>
+            <LogoContainer>
+                <LogoIcon 
+                    size={64} 
+                    color={theme.colorSubPrimary()} 
+                />
+                <h1>
+                    Catalogue<br/>
+                    Uml
+                </h1>
+            </LogoContainer>
+        </HeaderContainer>
     )
 };
 
 export default Header;
+
+const HeaderContainer = styled.header`
+    width: 100%;
+    height: 100px;
+    background-color: ${({ theme }) => theme.colorPrimary()};
+    border-bottom: 1.5px solid ${({ theme }) => theme.colorSubSecondary()};
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+`;
+
+const LogoContainer = styled.div`
+  width: 15%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  position: relative;
+  overflow: hidden;
+
+  h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.colorSubPrimary()};
+    font-family: 'Archivo Black', sans-serif;
+    letter-spacing: 1.2px;
+    text-transform: uppercase;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 15px;
+    bottom: 15px;
+    right: 0;
+    border-right: 1.5px solid ${({ theme }) => theme.colorSubSecondary()};
+  }
+`;
