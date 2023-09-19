0fcde18db70b97cd4c88cf9ea09ef5accac3e4d4
Thomas <thomas.mortelette@outlook.fr>
2023-09-19 11:39:42 +0200

    Ajout de react-icons et de la page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+import React from "react";
+import styled from "styled-components";
 
 const App = () => {
 
   return (
-    <>
+    <AppContainer>
 
-    </>
+    </AppContainer>
   )
 }
 
-export default App
+export default App;
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colorPrimary()};
+`;
