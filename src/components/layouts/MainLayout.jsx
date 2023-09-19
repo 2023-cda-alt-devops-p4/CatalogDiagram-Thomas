07c01699b70441d321b9b9cad3a80172f405efad
Thomas <thomas.mortelette@outlook.fr>
2023-09-19 13:18:19 +0200

    feat: Ajout du layout principale, header et du footer.

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 import { Outlet } from "react-router-dom";
+import { Header, Footer } from "../partials";
 
 const MainLayout = () => {
 
     return(
-        <main>
-            <Outlet />
-        </main>
+        <>
+            <Header />
+            <StyledMain>
+                <Outlet />
+            </StyledMain>
+            <Footer />
+        </>
     )
 }
 
 export default MainLayout;
+
+const StyledMain = styled.main`
+    width: 100%;
+    min-height: 100vh;
+`;
