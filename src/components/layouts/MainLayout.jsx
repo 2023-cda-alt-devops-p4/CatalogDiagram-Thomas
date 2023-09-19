97b4bc2c5dab0b0161c45fda53c14e614935b286
Thomas <thomas.mortelette@outlook.fr>
2023-09-19 16:47:09 +0200

    feat: Modification de la structure de MainLayout

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "../partials";
+import { SideBar } from "../sideBar";
 
 const MainLayout = () => {
+
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
     return(
         <>
             <Header />
             <StyledMain>
-                <Outlet />
+                <SideBar 
+                    isCollapsed={isCollapsed} 
+                    setIsCollapsed={setIsCollapsed} 
+                />
+                <RouteContent>
+                    <Outlet />
+                </RouteContent>
             </StyledMain>
             <Footer />
         </>
@@ -22,4 +31,14 @@ export default MainLayout;
 const StyledMain = styled.main`
     width: 100%;
     min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    padding-top: 100px;
+    padding-left: 400px;
+`;
+
+const RouteContent = styled.div`
+    flex-grow: 1;
+    height: 100%;
+    display: flex;
 `;
