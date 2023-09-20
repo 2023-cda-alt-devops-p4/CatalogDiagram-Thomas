e953071ce5749409f989fe0a1722ea8205203de9
Thomas <thomas.mortelette@outlook.fr>
2023-09-20 13:50:16 +0200

    feat: Modification de l'emplacement du ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { Routes, Route } from "react-router-dom";
 import routes from "./Routes";
+
+import * as themes from "./themes";
 
 const App = () => {
 
   return (
-    <AppContainer>
-      <Routes>
-        {routes.map((route, routeIndex) => ((
-          <Route 
-            key={`route-${routeIndex}`} 
-            path={route?.path} 
-            element={route?.element}
-          >
-            {route?.childrens.map((childrenRoute, childrenRouteIndex) => ((
-                <Route 
-                  key={`route-${routeIndex}-${childrenRouteIndex}`} 
-                  path={childrenRoute?.path} 
-                  element={childrenRoute?.element} 
-                />
-            )))}
-          </Route>
-        )))}
-      </Routes>
-    </AppContainer>
+    <ThemeProvider theme={themes.Default}>
+      <AppContainer>
+        <Routes>
+          {routes.map((route, routeIndex) => ((
+            <Route 
+              key={`route-${routeIndex}`} 
+              path={route?.path} 
+              element={route?.element}
+            >
+              {route?.childrens.map((childrenRoute, childrenRouteIndex) => ((
+                  <Route 
+                    key={`route-${routeIndex}-${childrenRouteIndex}`} 
+                    path={childrenRoute?.path} 
+                    element={childrenRoute?.element} 
+                  />
+              )))}
+            </Route>
+          )))}
+        </Routes>
+      </AppContainer>
+    </ThemeProvider>
   )
 }
 
