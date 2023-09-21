24109c583239464f6dc0098ca326d66b03ac24ce
Thomas <thomas.mortelette@outlook.fr>
2023-09-21 12:02:48 +0200

    feat: Ajout de plusieurs routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { MainLayout } from "./components/layouts";
-import { HomePage } from "./pages";
+import { HomePage, UmlPage } from "./pages";
 
 const routes = [
     {
         path: '/',
         element: <MainLayout />,
         childrens: [
-            { path: '/', element: <HomePage /> }
+            { path: '/', element: <HomePage /> },
+            { path: '/uml-structs/:uuid', element: <UmlPage /> },
+            { path: '/uml-behaviors/:uuid', element: <UmlPage /> }
         ]
     }
 ];
