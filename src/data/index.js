db12282273f74e3002deb6709a700814cce9d96f
Thomas <thomas.mortelette@outlook.fr>
2023-09-30 12:03:06 +0200

    feat: Ajout et mise à jour des données

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,9 @@
 import diagramsUmlStructs from "./diagrams_uml_structs.json";
 import diagramsUmlBehaviors from "./diagrams_uml_behaviors.json";
+import diagramsMerise from "./diagrams_merise.json";
 
 export {
     diagramsUmlStructs,
-    diagramsUmlBehaviors
+    diagramsUmlBehaviors,
+    diagramsMerise
 }
